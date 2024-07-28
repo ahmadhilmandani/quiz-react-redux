2c92e23f1e5cb35df4964a26db14e72ee7643809
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-07-28 16:53:34 +0700

    ADD create our very first redux with TS 😂

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+import { incrementByAmount, increment } from './state/counter/counterSlice.ts'
+import { useAppSelector, useAppDispatch } from './hooks.ts'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useAppSelector((state) => state.counter.value)
+  const dispatch = useAppDispatch()
 
   return (
     <>
@@ -17,17 +21,14 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="card flex gap-10 bg-red-50 items-center justify-center">
+        <button onClick={() => dispatch(increment())}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={() => dispatch(incrementByAmount(10))}>
+          count by 10 {count}
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
