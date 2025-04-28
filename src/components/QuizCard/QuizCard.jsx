85ee23bcd11f4a1c95e36c66462dceead926e1b7
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2025-04-28 23:45:54 +0700

    delete unused file

--- src/components/QuizCard/QuizCard.jsx
+++ src/components/QuizCard/QuizCard.jsx
@@ -2,8 +2,6 @@ import PropTypes from "prop-types";
 import AnswerOpt from "../AnswerOpt/AnswerOpt";
 
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-
 function decodeHtmlEntities(text) {
   const textArea = document.createElement('textarea');
   textArea.innerHTML = text;
